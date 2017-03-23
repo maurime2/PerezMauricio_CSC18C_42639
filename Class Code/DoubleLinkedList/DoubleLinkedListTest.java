5cf0641c1cb90d3c257e030e9911fe076a11b233
Mauricio S. Perez <maurime2@gmail.com>
2017-03-22 20:04:43 -0700

    Signed-off-by: Mauricio S. Perez <maurime2@gmail.com>

---
+++ Class Code/DoubleLinkedList/DoubleLinkedListTest.java
@@ -0,0 +1,37 @@
+public class DoubleLinkedListTest
+{
+	public static void main(String[] args)
+	{
+		// Create a DoubleLinkedList of up to 20 integers (Integer is the wrapper class for int)
+		DoubleLinkedList<Integer> dll_of_integers = new DoubleLinkedList();
+
+		System.out.println("Demonstrating dll_of_integers...");
+		System.out.println("Calling insertAtNext(5)...");
+		dll_of_integers.insertAtNext(5);
+		System.out.print("Content of dll_of_integers: ");
+		dll_of_integers.display();
+		System.out.printf("\n");
+		System.out.println("Calling insertAtNext(2)...");
+		dll_of_integers.insertAtNext(2);
+		System.out.print("Content of dll_of_integers: ");
+		dll_of_integers.display();
+		System.out.printf("\n");
+		System.out.println("Calling insertAtNext(12)...");
+		dll_of_integers.insertAtNext(12);
+		System.out.print("Content of dll_of_integers: ");
+		dll_of_integers.display();
+		System.out.printf("\n");
+		System.out.println("Calling insertAtNext(8)...");
+		dll_of_integers.insertAtNext(8);
+		System.out.println("Insert onto end of double linked list: 0 to 999...");
+		for(int i=0;i<1000;i++)
+		{
+			dll_of_integers.insertAtNext(i);
+		}
+		System.out.print("Content of dll_of_integers: ");
+		dll_of_integers.display();		
+		System.out.printf("\n\n");		
+		
+		
+	}
+}
